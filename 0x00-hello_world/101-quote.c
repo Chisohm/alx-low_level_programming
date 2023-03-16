65e0a99cf32af221e23c48f6332c7bf2d9dc8254
Chisohm <catherinechisom81@gmail.com>
2023-03-16 14:53:35 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,15 @@
 #include <unistd.h>
 
 /**
-* main - Entry
-* Return: 1
+* main - print quote
+* 
+* Description: print Dora's quote
+* Return: Always 1 (Success)
 */
+
 int main(void)
-{
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n,59);
+{	
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n, 59);
 	return (1);
 }	
 
