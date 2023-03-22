25e3e2aac401ec25af22f829090a97b1f0d99ce0
Chisohm <catherinechisom81@gmail.com>
2023-03-21 21:46:22 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,33 +11,10 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
+			printf("%d, ", n);
 	}
 	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }
