7f3eb7c3e0d6ffda91d73bdbd35ab2bed4392f3e
Chisohm <catherinechisom81@gmail.com>
2023-03-21 21:04:38 -0700

    modified 7modified

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,13 @@
 
 int print_last_digit(int n)
 {
-	int 1;
+	int last;
 
-	1 = n % 10;
-	if (1 < 0)
+	last = n % 10;
+	if (last < 0)
 	{
-		1 = 1 * -1;
-	{
-		_putchar(1 + '0');
-		return (1);
+		last = last * -1;
 	}
+	_putchar(last + '0');
+	return (last);
 }
