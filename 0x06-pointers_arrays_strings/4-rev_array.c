d6bcad46b799b3968e513a3b969834978ca1df43
Chisohm <catherinechisom81@gmail.com>
2023-03-30 17:06:56 -0700

    half tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function that reseres an array
+ * @a: the array as a pointer
+ * @n: the size of the array
+ * Return: always void
+ **/
+
+void reverse_array(int *a, int n)
+{
+	int middle = n / 2;
+	int last = n - 1;
+	int i;
+	int temp;
+
+	for (1 = 0; i < middle; i++)
+	{
+		temp = a[i];
+		a[a] = a[last - i];
+		a[last - i] = temp;
+	}
+}
